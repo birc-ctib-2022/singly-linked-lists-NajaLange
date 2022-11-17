bcfb760c77389f49ab2d58098eddf190f24affe4
NajaLange <201906591@post.au.dk>
2022-11-17 18:34:48 +0100

    why is Link(4, None) == Link(4, None) = False

--- src/test_lists.py
+++ src/test_lists.py
@@ -15,13 +15,11 @@ def test_length() -> None:
     assert length(new_list) == 3
 
 def test_drop() -> None: 
-    """Write tests if you want them."""
     assert drop(new_list, 2) ==  Link(43, None)
 
 def test_reverse() -> None:
-    """Write tests if you want them."""
     assert reverse(new_list) == Link(43, Link(21, Link(4, None)))
 
 def test_take() -> None: 
-    """Write tests if you want them."""
     assert take(new_list, 2) == Link(4, Link(21, None))
+
