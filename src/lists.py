129bb3660799c8d0a74cfdf6816efb53bf0a5b5d
NajaLange <201906591@post.au.dk>
2022-11-17 18:33:39 +0100

    why is Link(4, None) == Link(4, None) = False

--- src/lists.py
+++ src/lists.py
@@ -25,6 +25,16 @@ class Link(Generic[T]):
 LList = Optional[Link[T]]  # A list is just a reference to the head or None
 
 
+new_list = Link(21, Link(43, None))
+#print(new_list)
+
+new_list = Link(3, new_list)
+#print(new_list)
+
+#print(new_list.tail)
+
+
+
 def length(x: LList[T]) -> int:
     """
     Get the length of x.
@@ -36,8 +46,13 @@ def length(x: LList[T]) -> int:
     >>> length(Link(1, Link(2, None)))
     2
     """
-    ...
-
+    count = 0
+    while x != None:
+        count += 1
+        x = x.tail
+    return count
+    
+#print(length(new_list))
 
 def drop(x: LList[T], k: int) -> LList[T]:
     """
@@ -52,24 +67,17 @@ def drop(x: LList[T], k: int) -> LList[T]:
     >>> drop(Link(1, Link(2, None)), 1)
     Link(2, None)
     """
-    ...
 
+    for i in range(k):
+        if x == None: 
+            return None
+        x = x.tail 
+    return x 
 
-def take(x: LList[T], k: int) -> LList[T]:
-    """
-    Return a list with the first k elements in x.
+#print(drop(new_list, 2))
+#print(drop(None, 1))
+#print(drop(Link(1, None), 1))
 
-    If length(x) < k, return the full list. You decide whether you
-    want to return a copy of x or the original list.
-
-    >>> take(None, 1) is None
-    True
-    >>> take(Link(1, None), 1)
-    Link(1, None)
-    >>> take(Link(1, Link(2, Link(3, None))), 2)
-    Link(1, Link(2, None))
-    """
-    ...
 
 
 def reverse(x: LList[T]) -> LList[T]:
@@ -87,4 +95,44 @@ def reverse(x: LList[T]) -> LList[T]:
     >>> reverse(Link(1, Link(2, Link(3, None))))
     Link(3, Link(2, Link(1, None)))
     """
-    ...
+    rev = None 
+    while x != None: 
+        rev = Link(x.head, rev)
+        x = x.tail 
+    return rev
+
+#print(reverse(new_list))
+
+
+def take(x: LList[T], k: int) -> LList[T]:
+    """
+    Return a list with the first k elements in x.
+
+    If length(x) < k, return the full list. You decide whether you
+    want to return a copy of x or the original list.
+
+    >>> take(None, 1) is None
+    True
+    >>> take(Link(1, None), 1)
+    Link(1, None)
+    >>> take(Link(1, Link(2, Link(3, None))), 2)
+    Link(1, Link(2, None))
+    """
+
+    res = None
+    while True: 
+        if x == None: 
+            return res 
+        if k == 0:
+            return reverse(res)
+        x, k, res = x.tail, k-1, Link(x.head, res)
+    
+
+#print(new_list)
+#print(take(Link(1, Link(2, Link(3, None))), 2))
+
+print(drop(new_list, 2))
+print(drop(new_list, 2) ==  Link(43, None))
+
+if Link(43, None) ==  Link(43, None): 
+    print(True)
--- src/test_lists.py
+++ src/test_lists.py
@@ -1,6 +1,27 @@
 """List tests."""
 
+from lists import (
+    length,
+    drop,
+    reverse,
+    take,
+    Link
+)
 
-def test_me() -> None:
+new_list = Link(4, Link(21, Link(43, None)))
+
+def test_length() -> None:
     """Write tests if you want them."""
-    assert 2 + 2 == 4
+    assert length(new_list) == 3
+
+def test_drop() -> None: 
+    """Write tests if you want them."""
+    assert drop(new_list, 2) ==  Link(43, None)
+
+def test_reverse() -> None:
+    """Write tests if you want them."""
+    assert reverse(new_list) == Link(43, Link(21, Link(4, None)))
+
+def test_take() -> None: 
+    """Write tests if you want them."""
+    assert take(new_list, 2) == Link(4, Link(21, None))
